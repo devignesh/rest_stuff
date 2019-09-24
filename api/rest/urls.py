1a18934cdcc89c24fbca4ccb85c0f67c0f90ea34
devignesh <vinesh1865@gmail.com>
2019-09-24 10:48:42 +0530

    adding urls

---
+++ api/rest/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+from django.conf.urls import url,include
+from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+from rest_framework_jwt.views import (obtain_jwt_token,
+                                       verify_jwt_token,
+                                       refresh_jwt_token)
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest import views
+app_name = "rest"
+
+urlpatterns = [
+    
+    url('token_refresh/?(?P<user_id>[^/]+)/$', views.RefreshToken.as_view()),
+    
+    
+]
